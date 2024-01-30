e1152e6a8edad2c4f211956fe72a01049b9e6c46
AJ Westley <alexanderjwestley@gmail.com>
2024-01-30 22:12:25 +0200

    changed gcd to euclids algo

--- maths.pl
+++ maths.pl
@@ -9,10 +9,5 @@ has_divisor(N, P) :- 0 is N mod P, !.
 has_divisor(N, P) :- N > P + 1, has_divisor(N, P + 1).
 
 % Greatest common divisor
-gcd(X, Y, Result) :- X >= Y, gcd_aux(X, Y, X, Result), !.
-gcd(X, Y, Result) :- gcd_aux(X, Y, Y, Result).
-
-gcd_aux(X, Y, Current, Result) :- mutual_divisor(X, Y, Current), Result is Current, !.
-gcd_aux(X, Y, Current, Result) :- gcd_aux(X, Y, Current - 1, Result).
-
-mutual_divisor(X, Y, D) :- 0 is X mod D, 0 is Y mod D.
+gcd(X, 0, X) :- X > 0, !.
+gcd(X, Y, Result) :- Y > 0, Z is X mod Y, gcd(Y, Z, Result).
