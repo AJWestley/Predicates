27e6b23e354aae6daa05ddad1af1ecc44c0dae95
AJ Westley <alexanderjwestley@gmail.com>
2024-01-30 19:46:10 +0200

    Extra traversal base case

--- binary_tree.pl
+++ binary_tree.pl
@@ -10,16 +10,19 @@ count_leaves(t(_, nil, nil), 1):-!.
 count_leaves(t(_, L, R), Result) :- count_leaves(L, LCount), count_leaves(R, RCount), Result is LCount + RCount.
 
 % Inorder traversal
+inorder(nil, []):-!.
 inorder(t(X, nil, nil), [X]):-!.
 
 inorder(t(X, L, R), Total) :- inorder(L, Left), inorder(R, Right), append(Left, [X], P), append(P, Right, Total).
 
 % Preorder traversal
+preorder(nil, []):-!.
 preorder(t(X, nil, nil), [X]):-!.
 
 preorder(t(X, L, R), [X | Total]) :- preorder(L, Left), preorder(R, Right), append(Left, Right, Total).
 
 % Postorder traversal
+postorder(nil, []):-!.
 postorder(t(X, nil, nil), [X]):-!.
 
 postorder(t(X, L, R), Total) :- postorder(L, Left), postorder(R, Right), append(Left, Right, P), append(P, [X], Total).
